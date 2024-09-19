80c4cc03ddeec2e534ac11064dba2bce7cda0d0b
jkam <jpkamdem@proton.me>
2024-09-19 21:59:47 +0200

    initialisation du template, première tâche :'afficher un message si liste de tâche vide'

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<div v-if="todos.length === 0">Vous n'avez aucune tâche à effectuer.</div>
+<form @submit.prevet="addTodo">
+  <fieldset role="group">
+    <input type="text" placeholder="Veuillez écrire vos tâches ici...">
+    <button>Ajouter</button>
+  </fieldset>
+</form>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import { ref } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const todos = ref([]);
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
