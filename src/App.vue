fa47f86b3c2e21c33cf22f40c5252923e2f62920
jkam <jpkamdem@proton.me>
2024-09-19 22:07:29 +0200

    ajout de la liste

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-<div v-if="todos.length === 0">Vous n'avez aucune tâche à effectuer.</div>
-<form @submit.prevent="addTodo">
-  <fieldset role="group">
-    <input type="text" placeholder="Veuillez écrire vos tâches ici..." v-model="newTodo">
-    <button>Ajouter</button>
-  </fieldset>
-</form>
+  <form @submit.prevent="addTodo">
+    <fieldset role="group">
+      <input type="text" placeholder="Veuillez écrire vos tâches ici..." v-model="newTodo">
+      <button>Ajouter</button>
+    </fieldset>
+  </form>
+  <div v-if="todos.length === 0">Vous n'avez aucune tâche à effectuer.</div>
+  <div v-else>
+    <ul>
+      <li v-for="todo in todos" :key="todo.date">
+        {{ todo.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
