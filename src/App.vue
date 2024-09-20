8f842c619264961dff8baa3e723c1ab36953e6c4
jkam <jpkamdem@proton.me>
2024-09-20 16:22:20 +0200

    ajout de commentaires, nettoyage de certains trucs

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,65 @@
 <template>
   <form @submit.prevent="addTodo">
     <fieldset role="group">
-      <input type="text" placeholder="Veuillez écrire vos tâches ici..." v-model="newTodo">
+      <!-- v-model permet d'assigner un champ input à une variable réactive (ref) -->
+      <input type="text" placeholder="Veuillez écrire vos tâches ici..." v-model="newTodo" />
+      <!-- Vérification de la valeur du champ de texte, pour éviter les espaces -->
       <button :disabled="newTodo.trim() === ''">Ajouter</button>
     </fieldset>
   </form>
+  <!-- Affichage conditionnel, soit l'un soit l'autre -->
   <div v-if="todos.length === 0">Vous n'avez aucune tâche à effectuer.</div>
   <div v-else>
     <ul>
-      <li v-for="todo in sortedTodos" :key="todo.date" :class="{ completed: todo.completed }" >
-        <input type="checkbox" v-model="todo.completed">
+      <!-- Boucle simple, classe dynamique vérifiant la valeur de la propriété completed -->
+      <li v-for="todo in sortedTodos" :key="todo.date" :class="{ completed: todo.completed }" @click="deleteTodo(todo)">
+        <!-- click.stop permet d'empêcher la fonction deleteTodo de fonctionner sur la checkbox -->
+        <input type="checkbox" v-model="todo.completed" @click.stop />
         {{ todo.title }}
       </li>
     </ul>
     <label>
-      <input type="checkbox" v-model="hideCompleted">
+      <input type="checkbox" v-model="hideCompleted" />
       Masquer les éléments
     </label>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 
 const todos = ref([]);
-const newTodo = ref('');
+const newTodo = ref("");
 const hideCompleted = ref(false);
 
 const addTodo = () => {
-  if (newTodo.value.trim() === '') {
-    return;
+  if (newTodo.value.trim() === "") { // vérification de la valeur du champ de texte, comme au-dessus
+    return; 
   }
   todos.value.push({
     title: newTodo.value.trim(),
     completed: false,
     date: Date.now(),
   });
-  newTodo.value = '';
-}
+  newTodo.value = ""; // Réinitialisation du champ de texte
+};
 
 const deleteTodo = (todo) => {
-  todos.value = todos.value.filter(t => t.date !== todo.date);
-}
+  todos.value = todos.value.filter((t) => t.date !== todo.date); // Date.now() est unique à chaque tâche, même celle avec le même nom, c'est plus précis
+};
 
-const sortedTodos = computed(() => {
-  console.log('demo');
-    const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed);
-    if (hideCompleted.value) {
-      return sortedTodos.filter(t => t.completed === false);
-    }
-    return sortedTodos;
-})
+const sortedTodos = computed(() => { // optimisation grâce au computed, ne recalcule pas toute la fonction à chaque fois, uniquement lorsqu'une des valeurs dont l'objet dépend (ici todos.value & hideCompleted.value)
+  const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed);
+  if (hideCompleted.value) {
+    return sortedTodos.filter((t) => t.completed === false);
+  }
+  return sortedTodos;
+});
 </script>
 
 <style>
 .completed {
-  opacity: .5;
+  opacity: 0.5;
   text-decoration: line-through;
 }
 </style>
